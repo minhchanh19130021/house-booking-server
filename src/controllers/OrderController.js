ea090f9f596503e6eebdca5c6da7ac21c72d4b6b
NguyenVanPhuocHau-19130066 <92570580+NguyenVanPhuocHau-19130066@users.noreply.github.com>
2022-12-11 10:53:39 +0700

    update

--- src/controllers/OrderController.js
+++ src/controllers/OrderController.js
@@ -1,11 +1,8 @@
 
-import {
-  join
-} from "path";
+
 import Order from "../models/Order";
 import OrderDetail from "../models/OrderDetail";
-import OrdersDetailsController from "../controllers/OrdersDetailsController";
-import OrderId from "../controllers/OrderId"
+
 import {
   Types
 } from "mongoose";
@@ -15,52 +12,6 @@ require("dotenv").config();
 
 
 let bookingHome = async (req, res) => {
-
-  //   let sampleOrder = {
-  //   "_id": new Types.ObjectId(),    
-  //   "hid":  Types.ObjectId("637edcdc1a62375c4f241e39"),
-  //   "uid":  Types.ObjectId("638ee48ab1ea3871b675269e"),
-  //   "create_date":  new Date().toISOString(),
-  //   "total_price": 1000,
-  //   "is_review": false // auto false
-  // }
-
-  // let sampleOrderDetail= {
-  // "_id": new Types.ObjectId(),    
-  // "uid":  Types.ObjectId("638ee48ab1ea3871b675269e"),
-  // "payment_method": "paypal",
-  // "checkin":  new Date().toISOString(),
-  // "checkout":  new Date().toISOString(),
-  // "number_visitor": {
-  //   "adults": 1,
-  //   "child": 1,
-  //   "baby": 1,
-  //   "pet": 1,
-  // },
-  // "voucher": [],
-  // "price": 1,
-  // "oid": "",
-  // }
-
-
-  //   let order = new Order(sampleOrder);
-  //   let newOrder =  order.save(); 
-  //   newOrder.then(o=>{
-  //     console.log(o);
-  //     sampleOrderDetail.oid = o._id;
-  //     console.log(sampleOrderDetail);
-  //     let orderDetail = new OrderDetail(sampleOrderDetail);
-  //     orderDetail.save(function(err, orderDetail){
-  //       if (err) return console.error(err);
-  //       else {
-  //         return res.status(200).json({
-  //           success: true,
-  //           data: "oke",
-  //         });
-  //       }
-  //     })
-  //   })
-
 
   var ido = new Types.ObjectId();
   try {
@@ -86,21 +37,7 @@ let bookingHome = async (req, res) => {
   });
   await newOrder.save();
 
-  // newOrder.then(o => {
-  //   console.log(o);
-  //   // sampleOrderDetail.oid = o._id;
-  //   // console.log(sampleOrderDetail);
-  //   // let orderDetail = new OrderDetail(sampleOrderDetail);
-  //   newOrderDetail.save(function (err, orderDetail) {
-  //     if (err) return console.error(err);
-  //     else {
-  //       return res.status(200).json({
-  //         success: true,
-  //         data: "oke",
-  //       });
-  //     }
-  //   })
-  // })
+
 
   await newOrderDetail.save();
 
@@ -116,30 +53,10 @@ let bookingHome = async (req, res) => {
 }
 };
 
-// let getAllIdOrders = async (req, res, next) => {
-//   let query = Order.find({});
-//   query.exec(function (err, orders) {
-//     var result = [];
-//     orders.forEach(function (orders) {
-//       result.push(orders._id);
-//     });
-//     return res.status(200).json({
-//       success: true,
-//       data: result,
-//     });
-//   })
-// };
 
 
 
 
-module.exports = {
-  bookingHome,
-
-};
-=======
-import Order from '../models/Order';
-import mongoose from 'mongoose';
 
 let getOrderByIdUser = async (req, res) => {
     try {
@@ -406,6 +323,7 @@ let getOrderByHomeCategoriesOneYear = async (req, res) => {
 };
 
 module.exports = {
+    bookingHome,
     getOrderByIdUser,
     getOrderByIdUserAndDate,
     getOrderByIdUserAndMonth,
